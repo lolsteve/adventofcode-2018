1bcf361f74cf7ef079fd38b536bbd4ddec6ee152
Steven Wallace <steven@thewallaces.ca>
2018-12-05 01:29:26 -0800

    Refactored day 1 and completed part 2

--- day1/frequency.py
+++ day1/frequency.py
@@ -1,12 +1,25 @@
-frequency = 0
+def main():
+    frequencies = []
+    frequency = 0
+    seen = set([0])
 
-with open('input.txt') as file:
-    for line in file:
-        sign = line[:1]
-        number = int(line[1:])
-        if sign is '+':
-            frequency += number
-        else:
-            frequency -= number
+    with open('input.txt') as file:
+        for line in file:
+            change = int(line.strip())
+            frequencies.append(change)
 
-print(frequency)
+    for change in frequencies:
+        frequency += change
+        seen.add(frequency)
+    print('Sum of frequencies: {}'.format(frequency))
+
+    while True:
+        for change in frequencies:
+            frequency += change
+            if frequency in seen:
+                print('First repeated frequency: {}'.format(frequency))
+                return
+            seen.add(frequency)
+
+if __name__ == '__main__':
+    main()
