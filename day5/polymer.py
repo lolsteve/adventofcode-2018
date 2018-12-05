31c9e2d1560ed2d69aaa94a54ee010a949c2bf94
Steven Wallace <steven@thewallaces.ca>
2018-12-05 00:48:36 -0800

    day 5 part 2

--- day5/polymer.py
+++ day5/polymer.py
@@ -1,4 +1,4 @@
-DIST_BETWEEN_LOWER_AND_UPPER = abs(ord('a') - ord('A'))
+DIST_BETWEEN_LOWER_AND_UPPER = abs(ord('A') - ord('a'))
 
 def do_react(a, b):
     return abs(ord(a) - ord(b)) == DIST_BETWEEN_LOWER_AND_UPPER
@@ -25,6 +25,19 @@ def main():
     reacted_polymer = run_polymer(polymer[:])
 
     print('Reacted polymer length: {}'.format(len(reacted_polymer)))
+
+    shortest = len(polymer)
+    for i in range(ord('A'), ord('Z')+1):
+        upper_case = chr(i)
+        lower_case = chr(i + DIST_BETWEEN_LOWER_AND_UPPER)
+
+        filtered_polymer = [unit for unit in polymer if not (unit == lower_case or unit == upper_case)]
+
+        reacted_filtered_polymer = run_polymer(filtered_polymer[:])
+        if len(reacted_filtered_polymer) < shortest:
+            shortest = len(reacted_filtered_polymer)
+
+    print('Shortest filtered polymer: {}'.format(shortest))
 
 if __name__ == '__main__':
     main()
